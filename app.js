ea7b6ffabe4148a85fd80ef122ddf2fd3dcfb9b8
DvaeFroot <ddjimenez2@up.edu.ph>
2026-01-15 08:20:28 +0800

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+import { loadAnimations } from "./js/anim";
+import { scrollTrigger } from "./js/scroll";
+
+loadAnimations()
+// scrollTrigger('#intro-about-cloud-img', {
+//   rootMargin: '-300px'
+// })
---
+++ js/anim.js
@@ -0,0 +1,45 @@
+import { animate, spring } from "animejs";
+
+export function loadAnimations() {
+  animate('#intro-illustration-img',
+    {
+      y: ['8rem', '0'],
+      opacity: ['0', '1'],
+      ease: spring({ bounce: .35 }),
+    });
+
+  animate('#intro-character-img',
+    {
+      y: ['8rem', '0'],
+      opacity: ['0', '1'],
+      ease: spring({ bounce: .35 }),
+      delay: 100
+    });
+
+  animate('#intro-portfolio-img',
+    {
+      y: ['8rem', '0'],
+      opacity: ['0', '1'],
+      ease: spring({ bounce: .35 }),
+      delay: 200
+    });
+
+  animate('#intro-author-img',
+    {
+      y: ['8rem', '0'],
+      opacity: ['0', '1'],
+      ease: spring({ bounce: .35 }),
+      delay: 300
+    });
+
+
+  animate('#intro-about-cloud-img',
+    {
+      y: ['8rem', '0'],
+      opacity: ['0', '1'],
+      ease: spring({ bounce: .6 }),
+      rotate: ['-45', '0'],
+      duration: 1000,
+      delay: 800
+    });
+}
