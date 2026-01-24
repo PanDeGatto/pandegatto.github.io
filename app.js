92536b57df7714d1bb448083cc232c6e44163100
DvaeFroot <ddjimenez2@up.edu.ph>
2026-01-25 00:21:22 +0800

    to be continued

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { loadAnimations } from "./js/anim.js";
 import { scrollTrigger } from "./js/scroll.js";
+import "./index.css";
 
 loadAnimations()
 // scrollTrigger('#intro-about-cloud-img', {
